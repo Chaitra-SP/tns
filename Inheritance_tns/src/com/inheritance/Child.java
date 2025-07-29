467e75ce7955181ec4834c82c2d5d1dccb86f23a
Chaitra SP <chaitrasp073@gmail.com>
2025-07-29 18:34:27 +0530

    Add files via upload

---
+++ Inheritance_tns/src/com/inheritance/Child.java
@@ -0,0 +1,13 @@
+package com.inheritance;
+
+public class Child extends Father {
+String cycle="pink falsh";
+public static void main(String[] args) {
+	Child c=new Child();
+	System.out.println(c.car);
+	System.out.println(c.money);
+	System.out.println(c.cycle);
+	c.drinking();
+	c.reading();
+}
+}
---
+++ Inheritance_tns/src/com/inheritance/Father.java
@@ -0,0 +1,14 @@
+package com.inheritance;
+public class Father {
+int money=80000;
+String car="BMW";
+void drinking() {
+	System.out.println("coffe");
+	
+}
+void reading() {
+	System.out.println("news paper");
+	
+}
+
+}
